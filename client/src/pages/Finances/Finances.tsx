0059b4a0851cd8380c1e2afa56a61c8e01e04d62
Jackie Yu <jhaitaoyu14@gmail.com>
2025-08-09 22:14:57 +0100

    Update Finances.tsx
    remove the add row button at top navbar

--- client/src/pages/Finances/Finances.tsx
+++ client/src/pages/Finances/Finances.tsx
@@ -3,7 +3,7 @@ import Modal from "../../components/Modal.tsx";
 import useModalStore from "../../store/modal.store.ts";
 
 function Finances() {
-	const { isOpen, openModal } = useModalStore();
+	const { isOpen } = useModalStore();
 
 	return (
 		<div>
@@ -14,12 +14,6 @@ function Finances() {
 						<button type="button">Prev</button>
 						<button type="button">Next</button>
 					</div>
-					<div>
-						{/* Open a modal window to choose from the tables and add certain row data */}
-						<button type="button" onClick={() => openModal()}>
-							Add Row
-						</button>
-					</div>
 					<div>Calender</div>
 				</div>
 			</section>
@@ -29,9 +23,7 @@ function Finances() {
 				</article>
 				<article>
 					<h2>Investments</h2>
-					<Table
-						tableName="investments"
-					/>
+					<Table tableName="investments" />
 				</article>
 				<article>
 					<h2>Credit</h2>

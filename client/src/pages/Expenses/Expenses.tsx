f0eb75d76a50fff538519501f506552de5059abe
Jackie Yu <jhaitaoyu14@gmail.com>
2025-08-23 01:22:47 +0100

    Update Expenses.tsx
    toggle current table showing feature

--- client/src/pages/Expenses/Expenses.tsx
+++ client/src/pages/Expenses/Expenses.tsx
@@ -3,14 +3,35 @@ import Modal from "../../components/Modal.tsx";
 import useModalStore from "../../store/modal.store.ts";
 import { useEffect } from "react";
 import useTableStore from "../../store/table.store.ts";
+import useExpensesStore from "../../store/expenses.store.ts";
+import type { TableType } from "../../../../types/table.types.ts";
 
 function Expenses() {
 	const { isOpen } = useModalStore();
-	const {tables, getRows} = useTableStore()
+	const { getRows } = useTableStore();
+	const { currentTab, updateCurrentTab } = useExpensesStore();
+
+	const handleTabSelect = (e) => {
+		console.log(e.target.value);
+		updateCurrentTab(e.target.value);
+	};
+
+	const convertToTitle = (tab: TableType) => {
+		switch (tab) {
+			case "fixedPayments":
+				return "Fixed Payments";
+			case "investments":
+				return "Investments";
+			case "credit":
+				return "Credit";
+			default:
+				return "Table";
+		}
+	};
 
 	useEffect(() => {
-		getRows()
-	},[])
+		getRows();
+	}, [getRows]);
 
 	return (
 		<div>
@@ -22,28 +43,41 @@ function Expenses() {
 						<button type="button">Next</button>
 					</div>
 					<div>
-						<button>Fixed Payments</button>
-						<button>
+						<button
+							type="button"
+							value="fixedPayments"
+							className="mx-2 bg-blue-300 p-2 rounded-sm"
+							onClick={handleTabSelect}
+						>
+							Fixed Payments
+						</button>
+						<button
+							type="button"
+							value="investments"
+							className="mx-2 bg-blue-300 p-2 rounded-sm"
+							onClick={handleTabSelect}
+						>
 							Investments
 						</button>
-						<button>Credit</button>
+						<button
+							type="button"
+							value="credit"
+							className="mx-2 bg-blue-300 p-2 rounded-sm"
+							onClick={handleTabSelect}
+						>
+							Credit
+						</button>
 					</div>
 					<div>Calender</div>
 				</div>
 			</section>
 			<section>
-				<article className="w-full">
-					<h2>Fixed Payments</h2>
-					<Table tableName="fixedPayments" />
-				</article>
-				<article>
-					<h2>Investments</h2>
-					<Table tableName="investments" />
-				</article>
-				<article>
-					<h2>Credit</h2>
-					<Table tableName="credit" />
-				</article>
+				{currentTab && (
+					<article className="w-full">
+						<h2>{convertToTitle(currentTab)}</h2>
+						<Table tableName={currentTab} />
+					</article>
+				)}
 			</section>
 			{isOpen && <Modal />}
 		</div>

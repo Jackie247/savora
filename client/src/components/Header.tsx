2abf840ed5f432640db0608a475112b9cf7d7403
Jackie Yu <jhaitaoyu14@gmail.com>
2025-09-17 21:57:41 +0100

    add headers for mobile and desktop

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,45 @@
+import {
+	SignedIn,
+	SignedOut,
+	SignInButton,
+	UserButton,
+} from "@clerk/clerk-react";
+
+const Header = () => {
+	return (
+		<header id="header" className="flex justify-between p-8 align-middle items-center w-full">
+			<div id="logo">
+				<a href="/">
+					<span>Savora</span>
+				</a>
+			</div>
+			<nav id="navbar" className="">
+				<ul className="flex">
+
+					<li className="p-2 mx-2">
+						<a href="/">Dashboard</a>
+					</li>
+					<li className="p-2 mx-2">
+						<a href="/expenses">Expenses</a>
+					</li>
+					<li className="p-2 mx-2">
+						<a href="/options">Options</a>
+					</li>
+					<li className="p-2 mx-2">
+						<a href="/help">Help</a>
+					</li>
+				</ul>
+			</nav>
+
+			<SignedIn>
+				<UserButton />
+			</SignedIn>
+
+			<SignedOut>
+				<SignInButton />
+			</SignedOut>
+		</header>
+	);
+};
+
+export default Header;

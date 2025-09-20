a6e033f215cc7e92d2638d3374634584942f84c8
Jackie Yu <jhaitaoyu14@gmail.com>
2025-09-21 00:37:21 +0100

    Update app.ts
    add routes and clerk auth middleware

--- server/app.ts
+++ server/app.ts
@@ -1,19 +1,25 @@
 import dotenv from "dotenv";
 import express from "express";
 import expensesRouter from "./routes/expenses.ts";
+import userRouter from "./routes/user.js";
+import {
+	clerkMiddleware,
+	requireAuth,
+} from "@clerk/express";
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+app.use(clerkMiddleware());
 
 // Routes
-app.get("/", (req, res) => {
-	res.send("Home Page");
-});
-app.use("/api/expenses", expensesRouter);
+app.use("/api/user", requireAuth(), userRouter);
+app.use("/api/expenses", requireAuth(), expensesRouter);
+// app.use("/api/user", userRouter);
 // app.use('/api/expenses', expensesRouter)
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));

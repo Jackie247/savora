77036bb86aebf6e2381893cfa11ff307f97e6ef1
Jackie Yu <jhaitaoyu14@gmail.com>
2025-09-21 00:36:19 +0100

    update types files

--- types/table.types.ts
+++ types/table.types.ts
@@ -4,13 +4,14 @@ export type TableType = "fixedPayments" | "investments" | "credit";
 export interface TableRowData {
 	// stores the base schema for a table row
 	expenseType?: string; // keep track of which table the row is in
-	id: string;
+	id: number;
 	name?: string;
 	value?: number;
 	is_recurring?: boolean;
 	expense_date?: Date;
 	recurring_day?: number; // day of month for recurring expenses
 	recurring_interval?: string; // 'monthly', 'yearly', etc.
+	user_id: number;
 }
 
 export type NewRow = Omit<TableRowData, "id">;
